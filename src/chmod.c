4112ed125c605edbdc3df6d505d50eeb23fed509
fatzchang <fatz.zhang@gmail.com>
2021-05-04 17:42:57 +0000

    fopen

--- src/chmod.c
+++ src/chmod.c
@@ -2,25 +2,21 @@
 #include <string.h>
 #include <limits.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
-#include "output.h"
-#include "dl.h"
+#include "utils.h"
 
 int chmod(const char *path, mode_t mode)
 {
     char resolved_path[PATH_MAX];
-    if (realpath(path, resolved_path) == NULL) {
-        // get realpath failed, just use untouched path
-        memset(resolved_path, 0, PATH_MAX);
-        strcpy(resolved_path, path);
-    }
+    get_real_path(resolved_path, path);
 
     int (*real_chmod)(const char *, mode_t) = get_real_func("chmod");
 
     int rtn = real_chmod(path, mode);
     
     char buffer[MAX_MESSAGE_SIZE] = { 0 };
-    snprintf(buffer, MAX_MESSAGE_SIZE, "chmod(\"%s\", %o) = %d", resolved_path, mode, rtn);
+    snprintf(buffer, MAX_MESSAGE_SIZE, "chmod(\"%s\", %o) = %d", resolved_path, mode & (S_IRWXU | S_IRWXG | S_IRWXO), rtn);
     printline(buffer);
 
     return rtn;
